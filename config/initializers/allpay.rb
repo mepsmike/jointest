8cb2d9e56050deefeac286fbffa51976e63c56e4
mepsmike <mepsmike@gmail.com>
2015-08-30 00:47:23 +0800

    fix

--- config/initializers/allpay.rb
+++ config/initializers/allpay.rb
@@ -9,7 +9,6 @@ ActiveMerchant::Billing::Integrations::Allpay.setup do |allpay|
     allpay.hash_key    = '5294y06JbISpM5x9'
     allpay.hash_iv     = 'v77hoKGq4kWxNNIS'
   else
-    change to yours
     allpay.merchant_id = '2000132'
     allpay.hash_key    = '5294y06JbISpM5x9'
     allpay.hash_iv     = 'v77hoKGq4kWxNNIS'
